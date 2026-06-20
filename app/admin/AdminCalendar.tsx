e0b65573892a91cdd5701c6fbe7d079757a7e240
Aroussi <benaribiaaroussi@gmail.com>
2026-06-20 03:32:10 +0100

    fixed deployment errur

--- app/admin/AdminCalendar.tsx
+++ app/admin/AdminCalendar.tsx
@@ -2,10 +2,7 @@
 
 import { useMemo, useState } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
+import { format, parse, startOfWeek, getDay } from "date-fns";
 import { fr } from "date-fns/locale";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
